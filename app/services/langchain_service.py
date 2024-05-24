2f2add74a01f535d7dc3268c861dc3c529ebb91e
amolinam08 <a.molinam@uniandes.edu.co>
2024-05-24 13:19:44 -0500

    Import fixed

--- app/services/langchain_service.py
+++ app/services/langchain_service.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.memory import ChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables import RunnablePassthrough
